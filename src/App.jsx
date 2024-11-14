ce050bdea2836f80f033331872fc782ab5d2d5a8
jujudev7 <penna.julien@gmail.com>
2024-11-14 18:06:16 +0100

    todos.map() moved in return & setTodos modified

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,37 @@ export default function App() {
 
   const onSubmit = (formData) => {
     const todo = formData.get("todo");
-    todos.unshift(todo); // Vient placer le nouvel élément en haut de la pile
-    setTodos([...todos]); // Met à jour l'état avec la nouvelle valeur de todo
+    setTodos([todo, ...todos]); // Met à jour l'état avec la nouvelle valeur de todo
   };
 
-  const liTodos = todos.map((todo) => (
-    <li key={todo}>
-      <input
-        type="text"
-        defaultValue={todo}
-        name="todo"
-        className="border rounded-md w-4/5 mb-2 bg-slate-100 hover:bg-slate-200 px-4 py-3 flex-1"
-      />
-    </li>
-  ));
-
   return (
-    <div className="p-4 flex flex-col gap-4 max-w-lg mx-auto">
-      <h1 className="font-bold text-2xl">Ma To-do List 🗒️</h1>
+    <div className="mx-auto flex max-w-lg flex-col gap-4 p-4">
+      <h1 className="text-2xl font-bold">Ma To-do List 🗒️</h1>
       <form action={onSubmit} className="flex items-center gap-2">
         <input
           type="text"
           name="todo"
-          className="border rounded-md px-3 py-3 flex-1"
+          className="flex-1 rounded-md border px-3 py-3"
         />
         <button
           type="submit"
-          className="border rounded-md px-4 py-3 bg-zinc-800 hover:bg-black text-white"
+          className="rounded-md border bg-zinc-800 px-4 py-3 text-white hover:bg-black"
         >
           Ajouter
         </button>
       </form>
-      <ul>{liTodos}</ul>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>
+            <input
+              type="text"
+              defaultValue={todo}
+              name="todo"
+              className="mb-2 w-4/5 flex-1 rounded-md border bg-slate-100 px-4 py-3 hover:bg-slate-200"
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
