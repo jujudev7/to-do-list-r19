f78221c2bcce18e4c1fdeec4e2132cb6bec65ac7
jujudev7 <penna.julien@gmail.com>
2024-11-14 18:24:48 +0100

    delete added with id

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Manger" },
+    { id: 2, text: "Dormir" },
+  ]);
 
   const onSubmit = (formData) => {
     const todo = formData.get("todo");
-    setTodos([todo, ...todos]); // Met à jour l'état avec la nouvelle valeur de todo
+    setTodos([{ id: Date.now(), text: todo }, ...todos]); // Met à jour l'état avec la nouvelle valeur de todo
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
@@ -26,13 +33,17 @@ export default function App() {
       </form>
       <ul>
         {todos.map((todo) => (
-          <li key={todo}>
-            <input
-              type="text"
-              defaultValue={todo}
-              name="todo"
-              className="mb-2 w-4/5 flex-1 rounded-md border bg-slate-100 px-4 py-3 hover:bg-slate-200"
-            />
+          <li
+            key={todo.id}
+            className="mb-2 flex w-4/5 flex-1 items-center justify-between rounded-md border bg-slate-100 px-4 py-3 hover:bg-slate-200"
+          >
+            <span>{todo.text}</span>
+            <button
+              onClick={() => deleteTodo(todo.id)}
+              className="rounded-md border border-zinc-800 bg-white p-1 text-sm"
+            >
+              🗑️
+            </button>
           </li>
         ))}
       </ul>
